6ab19c6b409f224576451eea838bb6022d1d8a19
Riley Chase <riley.chase@rileychase.net>
2022-11-03 21:55:11 +1030

    Update CLI interface to find words and show the result

--- squaredle/__main__.py
+++ squaredle/__main__.py
@@ -2,7 +2,7 @@
 import pathlib
 import sys
 
-from . import prefix_tree, squaredle
+from . import prefix_tree, search, squaredle
 
 
 def main() -> None:
@@ -19,16 +19,33 @@ def main() -> None:
     if not game_file.is_file():
         print(f"Expected regular file at {game_file}", file=sys.stderr)
         sys.exit(1)
-    game_string = game_file.read_text().strip()
 
-    words = words_file.read_text("utf-8").split("\n")
-    print(f"Loaded {len(words)} from {words_file}", file=sys.stderr)
-
+    game_string = game_file.read_text("utf-8").strip()
+    words = [w for w in words_file.read_text("utf-8").split("\n") if w and len(w) > 3]
     tree = prefix_tree.build_prefix_tree(words)
-    print(f"Built prefix tree from {len(words)} words", file=sys.stderr)
-
     board = squaredle.load_game(game_string)
-    print("Loaded game board for " + game_string.replace("\n", "\\n"), file=sys.stderr)
+
+    print(
+        'Loaded Squaredle Game -- "' + game_string.replace("\n", " ") + '"',
+        file=sys.stderr,
+    )
+    print("", file=sys.stderr)
+
+    found_words = search.find_words(board, tree)
+
+    count = 0
+    for idx, words in enumerate(found_words):
+        count += len(words)
+        if words:
+            print(
+                f"From {board.nodes[idx].character.upper()} ({idx // 3}, {idx % 3}) "
+                f"there are {len(words)} words:",
+                file=sys.stderr,
+            )
+            print(", ".join([w.upper() for w in words]))
+            print("", file=sys.stderr)
+
+    print(f"Found {count} words in total", file=sys.stderr)
 
 
 if __name__ == "__main__":
